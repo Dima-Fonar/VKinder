dfd5a0dd315b765154c4bb435d022d01f7c310dd
Дмитрий Бабич <deity-08@mail.ru>
2022-12-20 22:32:41 +0300

    added data input format check

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from params import TOKEN_GROUP
 from vk_requests import get_user_info, get_user_search, get_photos, get_region, get_city
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
 
 db = ORM()
 
 vk = vk_api.VkApi(token=TOKEN_GROUP)
 longpoll = VkLongPoll(vk)
+
 
 
 def write_msg(user_id, message):
@@ -30,7 +33,7 @@ def write_msg_attachment(user_id, message, attachment):
     :return:
     """
     vk.method('messages.send',
-              {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), 'attachment': attachment})
+              {'user_id': user_id, 'message': message,'random_id': randrange(10 ** 7), 'attachment': attachment})
 
 
 def greeting(user_id):
@@ -80,6 +83,7 @@ def get_region_for_search_city_in_chat():
                 user_region = get_region(user_answer)
                 return user_region
 
+
 def get_city_for_search_in_chat(user_region_id):
     """
     принимает в переменную город в котором будет осуществлятся поиск
@@ -100,36 +104,44 @@ for event in longpoll.listen():
             request = event.text
             if request == "start":
                 user_info = get_user_info(event.user_id)
-                if 'city_id' not in user_info.keys():
+
+                if 'city_id' not in user_info.keys():  # Проверка, заполнено ли поле город в профиле пользователя
                     write_msg(event.user_id, f"У тебя в профиле не указан город! Давай заполним!\n"
                                              " Введи название региона в котором хочешь искать пару.")
                     user_region_id = get_region_for_search_city_in_chat()
-                    if user_region_id == 'Я не нашел такой регион, введи заново!':
-                        write_msg(event.user_id,
-                                  f"Я не нашел такой регион, введи заново!")
-                        user_region_id = get_region_for_search_city_in_chat()
-                    if len(str(user_region_id)) > 7:
-                        write_msg(event.user_id,
-                                  f"Я нашел несколько регионов с похожим названием, вот они:{user_region_id}")
-                        user_region_id = get_region_for_search_city_in_chat()
+                    while user_region_id == 'Я не нашел такой регион, введи заново!' or len(str(user_region_id)) > 7:
+                        if user_region_id == 'Я не нашел такой регион, введи заново!':
+                            write_msg(event.user_id,
+                                      f"Я не нашел такой регион, введи заново!")
+                            user_region_id = get_region_for_search_city_in_chat()
+                        else:
+                            write_msg(event.user_id,
+                                      f"Я нашел несколько регионов с похожим названием, вот они:{user_region_id}")
+                            user_region_id = get_region_for_search_city_in_chat()
                     write_msg(event.user_id, f"Введи название города в котором будем искать!")
                     user_city = get_city_for_search_in_chat(user_region_id)
-                    if user_city == 'Я не нашел такой город, введи заново!':
+                    while user_city == 'Я не нашел такой город, введи заново!':
                         write_msg(event.user_id,
                                   f"Я не нашел такой город, введи заново!")
                         user_city = get_city_for_search_in_chat(user_region_id)
                     user_info['city_id'] = user_city
                     write_msg(event.user_id, f"Все ОК! Я нашел такой город\n"
-                              "Чтобы не вводит город при каждом поиске, заполни свой профиль!")
+                                             "Чтобы не вводит город при каждом поиске, заполни свой профиль!")
 
-                if 'bdate' not in user_info.keys():
+                if 'bdate' not in user_info.keys():  # Проверка, заполнено ли поле дата рождения в профиле пользователя
                     write_msg(event.user_id,
-                              "У тебя в профиле не указана дата рождения! Введи свою дату рождения в формате "
-                              "день.месяц.год.(1.1.1900)")
+                              "У тебя в профиле не указана дата рождения! Введи год свое рождения")
                     user_bdate = get_bdate()
+                    while not user_bdate.isdigit() or 2004 < int(user_bdate):
+                        if user_bdate.isdigit():
+                            write_msg(event.user_id, "Тебе еще нет 18!")
+                            user_bdate = get_bdate()
+                        else:
+                            write_msg(event.user_id, "Ошибка ввода! Помоему ты вводишь не цифры! Пример: 1990")
+                            user_bdate = get_bdate()
                     user_info['bdate'] = user_bdate
 
-                if 'sex' not in user_info.keys():
+                if 'sex' not in user_info.keys():  # Проверка, заполнено ли поле пол в профиле пользователя
                     write_msg(event.user_id,
                               "У тебя в профиле не указан пол! Введи '1' если ты девушка!\n"
                               "введи '2' если ты парень!)")
@@ -143,6 +155,6 @@ for event in longpoll.listen():
                     if not db.search_id(user_id_to_db, event.user_id):  # проверка, отправлялась ли найденная
                         # страница  данному пользователю
                         db.add_user(user_id_to_db, event.user_id)  # добавление найденной страницы в БД
-                        photo = get_photos(user_id_to_db)   # получение трех лучших фото
-                        write_msg_attachment(event.user_id, f'https://vk.com/id{user_id_to_db}', photo)   # отправка
+                        photo = get_photos(user_id_to_db)  # получение трех лучших фото
+                        write_msg_attachment(event.user_id, f'https://vk.com/id{user_id_to_db}', photo)  # отправка
                         # ссылки и фото в чат
